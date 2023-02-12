3699c32d69ba681e0ee75d0897a747dfe82c7d64
OcularCreo <122617813+OcularCreo@users.noreply.github.com>
2023-02-11 20:35:36 -0500

    stylized the scene

--- public/js/test.js
+++ public/js/test.js
@@ -3,7 +3,7 @@
 //PROBABLY NEED TO ADD A CLASS TO THIS TOO!
 class assetInfo {
     constructor(id, model, rotation, scale, oriPos, height){
-        this.class      = "sandwhichIngredient interactive";
+        this.class      = "sandwichIngredient interactive";
         this.id         = id; 
         this.model      = model;
         this.rotation   = rotation; 
@@ -24,7 +24,7 @@ const chicken   = new assetInfo("chickIngred", "#chicken_model", "0 0 0", "0.02 
 
 let ingredList = [bread, cheese, tomato, meat, pizza, rat, chicken]
 
-let curY = 0;               //Helps track the current height of the sandwhich
+let curY = 0;               //Helps track the current height of the sandwich
 let lastIngred = bread;     //defaulting prevIngred to bread //tracks last selected asset info
 let cycleIngred = bread;    //variable to track previous asset on display info
 let previous_asset = makeIngred(bread, 0);    //variable to track previous asset on display
@@ -54,95 +54,98 @@ function makeIngred(ingredInfo){
 window.onload = function(){
 
     let scene       = document.querySelector("a-scene");    //scene
-    let btn_start   = document.getElementById("btn_start"); //button to start sandwhich
-    let btn_end     = document.getElementById("btn_end");   //button to finish sandwhich
+    let btn_start   = document.getElementById("btn_start"); //button to start sandwich
+    let btn_end     = document.getElementById("btn_end");   //button to finish sandwich
 
-    let sandNum = 0;    //tracks number of items that make up the sandwhich
+    let sandNum = 0;    //tracks number of items that make up the sandwich
     let added = 1;      //bool for when new ingredient has been added
-    let makingSand = 1; //tracksi if sandwhich is being made
-    let eaten = 0;
+    let makingSand = 0; //tracksi if sandwich is being made
 
-    //represents group/parent of the sandwhich and it's ingredients
-    let sandwhich = document.createElement("a-entity");  
-    sandwhich.setAttribute("id", "mainSandwhich");
-    sandwhich.setAttribute("class", "interactive");
-    sandwhich.setAttribute("position", {x:3, y:1.1, z:0});
-    sandwhich.setAttribute("sound", "src: #eating_sound; on:click;");
-    scene.append(sandwhich);
+    //represents group/parent of the sandwich and it's ingredients
+    let sandwich = document.createElement("a-entity");  
+    sandwich.setAttribute("id", "mainSandwich");
+    sandwich.setAttribute("class", "interactive");
+    sandwich.setAttribute("position", {x:3, y:1.1, z:0});
+    sandwich.setAttribute("sound", "src: #eating_sound; on:click;");
+    scene.append(sandwich);
 
     let randIngredInfo = ingredList[Math.floor(Math.random() * 6)];   //get a random ingredient info
     let randIngredAsset = makeIngred(randIngredInfo);                 //create the selected ingredient
 
-    //begin building sandwhich or add ingredients to sandwhich
+    //begin building sandwich or add ingredients to sandwich
     btn_start.addEventListener('click', function(){
         
-        
+        //when there is no sandwich made create the bread
         if(sandNum == 0){
             randIngredInfo = bread;
             randIngredAsset = makeIngred(randIngredInfo);
+            
+            //setup variables
             makingSand = 1;
             curY = 0;
+            sandNum = 0;
         }
 
+        //tracking when an ingredient has been added to the sandwich
         added = 1;
         sandNum++;
 
     });
 
-    //finish the sandwhich
+    //finish the sandwich
     btn_end.addEventListener('click', function(){
 
         //add the last piece of bread
         let lastBread = makeIngred(bread, sandNum);
-        sandwhich.removeChild(sandwhich.lastChild); //possibly remove this?
-        sandwhich.appendChild(lastBread);
+        sandwich.removeChild(sandwich.lastChild);
+        sandwich.appendChild(lastBread);
 
-        sandNum = 0;
-        makingSand = 0;
+        makingSand = 0; //turn of functions that make the sandwich
 
     });
 
-    //deletes the created sandwhich when clicked. This will be sort of like an "eating" action
-    sandwhich.addEventListener('click', function(){
+    //deletes the created sandwich when clicked. This will be sort of like an "eating" action
+    sandwich.addEventListener('click', function(){
         
-        //verifiy that the sandwhich isn't currently being constructed
+        //verifiy that the sandwich isn't currently being constructed
         if(makingSand == 0){
-            //delete all children of the sandwhich group
-            while(sandwhich.firstChild){
-                sandwhich.removeChild(sandwhich.firstChild);    
+            //delete all children of the sandwich group
+            while(sandwich.firstChild){
+                sandwich.removeChild(sandwich.firstChild);    
             }
+
+            sandNum = 0; //reset sandwich number
         }
 
-
     });
 
-    //cycles through sandwhich ingredients
+    //cycles through sandwich ingredients
     setInterval(function(){
 
-        //dont do anything if there is nothing in the sandwhich
-        if(!sandNum){
+        //dont do anything if there is nothing in the sandwich
+        if(!makingSand){
             return;
         }
 
-        //add the ingredient when the user clicks the button or if it's the first ingred (bread)
+        //add the ingredient when the user clicks the button
         if(added){
 
-            added = 0;
-            randIngredAsset = makeIngred(randIngredInfo);        //create the selected ingredient
-            sandwhich.append(randIngredAsset);
+            added = 0;                                      //flip added
+            randIngredAsset = makeIngred(randIngredInfo);   //create the selected ingredient
+            sandwich.append(randIngredAsset);               //
 
+            //determine the new current sandwhich height and store the current ingred info as lastIngred
             curY += (randIngredInfo.height / 2) + (lastIngred.height / 2);
             lastIngred = randIngredInfo;
 
         } else {
-            sandwhich.removeChild(sandwhich.lastChild);
+            sandwich.removeChild(sandwich.lastChild); //remove the ingredient
         }
 
         randIngredInfo = ingredList[Math.floor(Math.random() * 6)];   //get a random ingredient info
-        randIngredAsset = makeIngred(randIngredInfo);        //create the selected ingredient
-        sandwhich.append(randIngredAsset);
+        randIngredAsset = makeIngred(randIngredInfo);                 //create the selected ingredient
+        sandwich.append(randIngredAsset);                             //
 
-    }, 500);
-
+    }, 250);
     
 };
